908956fdfe25785f8dbd7ba0c95eec64690bc590
eeRaike <143718700+eeRaike@users.noreply.github.com>
2024-02-26 11:36:35 -0300

    doing exercises

--- Estrutura de Dados/exercicios forloops.js
+++ Estrutura de Dados/exercicios forloops.js
@@ -26,11 +26,62 @@ for (var _i = 0, text_1 = text; _i < text_1.length; _i++) {
     var i = text_1[_i];
     console.log(i);
 }
-// médios
+//5.
+console.log("\nEx.5");
+var soma = 0;
+for (var i = 0; i < 10; i++) {
+    if (i % 2 === 0) {
+        soma += i;
+        console.log(soma);
+    }
+}
+//6
+console.log("\nEx.6");
+var array3 = [112, 222, 34, 44, 45];
+var min = array3[0];
+for (var i = 0; i < array3.length; i++) {
+    if (min > array3[i]) {
+        min = array3[i];
+    }
+    console.log(min);
+}
+// médios (vou fazer depois to com sono demais pra usar o cérebro)
+//1. 
+console.log("\nEx.1");
+var primeArray = [10, 2, 3, 17, 100, 144, 22, 89, 97];
+function isPrime(num) {
+    if (num <= 1)
+        return false;
+    for (var i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    console.log(num, "is prime");
+    return true;
+}
+primeArray.forEach(isPrime);
+//2.
+console.log("\nEx.2");
+var text2 = "String teste muito zika B)";
+var vowels = text2.toLowerCase().split("");
+var vowelCount = 0;
+console.log(vowels);
+for (var i = 0; i < vowels.length; i++) {
+    if (vowels[i].match(/[aeiou]/g)) {
+        vowelCount++;
+        console.log(vowels[i], vowelCount);
+    }
+}
+/*
 //1.
 console.log("\nEx.1");
-var array2 = [1, 2, 3, 4, 5, 6];
-for (var i = 0; i < array2.length; i++) {
-    var a = array2[i] += array2[i + 1];
-    console.log(a / array2.length);
+
+const array2 = [1,2,3,4,5,6]
+
+for (let i = 0; i < array2.length; i++) {
+    let a = array2[i] += array2[i+1]
+    console.log(a / array2.length)
+
 }
+*/ 
--- Estrutura de Dados/exercicios forloops.ts
+++ Estrutura de Dados/exercicios forloops.ts
@@ -38,9 +38,89 @@ for (const i of text) {
     
 }
 
+//5.
+console.log("\nEx.5");
+let soma =  0
+for (let i = 0; i < 10; i++) {
+    if(i % 2 === 0){
+        soma += i
+        console.log(soma);
+        
+    }
+    
+}
 
-// médios
+//6
+console.log("\nEx.6");
 
+const array3 = [112,222,34,44,45]
+let min = array3[0]
+for (let i = 0; i < array3.length; i++) {
+    if(min > array3[i]){
+      min = array3[i]
+        
+    }
+    console.log(min);
+    
+    
+}
+
+
+
+// médios (vou fazer depois to com sono demais pra usar o cérebro)
+
+
+//1. 
+console.log("\nEx.1");
+
+const primeArray = [10,2,3,17,100,144,22,89,97]
+
+function isPrime(num:number): Boolean{
+    if(num <= 1) return false;
+
+    for (let i = 2; i < num; i++) {
+       if(num % i === 0){
+         return false;
+       }
+        
+    }
+    console.log(num ,"is prime");
+    return true;
+    
+    
+}
+
+primeArray.forEach(isPrime)
+
+//2.
+console.log("\nEx.2");
+
+    let text2 = "String teste muito zika B)"
+
+    let vowels = text2.toLowerCase().split("")
+    let vowelCount:number = 0
+    console.log(vowels);
+    
+
+
+    for (let i = 0; i < vowels.length; i++) {
+        if(vowels[i].match(/[aeiou]/g)){
+            vowelCount++
+            console.log(vowels[i],vowelCount);
+            
+        }
+    
+                
+    }
+
+
+
+
+
+
+
+
+/*
 //1.
 console.log("\nEx.1");
 
@@ -51,5 +131,4 @@ for (let i = 0; i < array2.length; i++) {
     console.log(a / array2.length) 
 
 }
-
-
+*/
